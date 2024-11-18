0c6395c24662cbe254887c987525898604056328
user <user@DESKTOP-GAP52DF>
2024-11-18 12:03:05 +0100

    Start.

--- MyCalendar/Form1.cs
+++ MyCalendar/Form1.cs
@@ -1,10 +1,38 @@
+﻿using Nager.Date.Models;
+using Nager.Holiday;
+using Nager.Date;
+
+
+//TODO: Der neue Kalender: Keinen Kalender sondern Buttons zum Navigieren; keine GridView, stattdessen interaktiver Kalender. 
+
+
 namespace MyCalendar
 {
+
     public partial class Form1 : Form
     {
+
         public Form1()
         {
-            InitializeComponent();
+
+
+            Size = new Size(900, 600);
+
+
+            
+            CreateUIControls();
+
         }
+
+      
+        private void CreateUIControls()
+        {
+
+           
+
+
+        }
+
+
     }
 }
